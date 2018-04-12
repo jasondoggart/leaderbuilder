8b4a48e4258350e8e787c45fd5f24d684d21ff25
Jason Doggart <jasondoggart@gmail.com>
2018-04-12 15:14:11 -0500

    Cleaned up clearance specs to stop them failing

--- spec/support/features/clearance_helpers.rb
+++ spec/support/features/clearance_helpers.rb
@@ -30,10 +30,6 @@ module Features
       click_button I18n.t("helpers.submit.user.create")
     end
 
-    def expect_user_to_be_signed_in
-      visit root_path
-      expect(page).to have_button I18n.t("layouts.application.sign_out")
-    end
 
     def expect_user_to_be_signed_out
       expect(page).to have_content I18n.t("layouts.application.sign_in")

6483c61119df5eb5b0c0d855d056e1625e4034f2
Jason Doggart <jasondoggart@gmail.com>
2018-04-23 21:30:40 -0400

    Add conditions for adding users
    Users can only be added if
    
    1) The have not previously been added.  This is checked by comparing the
    user's pc_id to all pc_ids currently stored.
    
    2) They have an email address in planning center.

--- app/models/fetch_users.rb
+++ app/models/fetch_users.rb
@@ -8,14 +8,17 @@ class FetchUsers
     require 'uri'
     require 'net/http'
 
-    people_url = URI("https://api.planningcenteronline.com/people/v2/people?where[last_name]=" + @last_name)
+    people_url = URI(
+      "https://api.planningcenteronline.com/people/v2/people?where[last_name]=" + @last_name
+    )
 
     http = Net::HTTP.new(people_url.host, people_url.port)
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(people_url)
     request["content-type"] = 'application/json'
-    request.basic_auth ENV['PLANNING_CENTER_APPLICATION_ID'], ENV['PLANNING_CENTER_SECRET']
+    request.basic_auth ENV['PLANNING_CENTER_APPLICATION_ID'],
+      ENV['PLANNING_CENTER_SECRET']
 
     people_list = http.request(request)
     people = JSON.parse(people_list.body)
@@ -24,12 +27,14 @@ class FetchUsers
     people_hash = Hash.new
     people['data'].each do |person|
       person_id = person['id']
-
-      email_url = URI("https://api.planningcenteronline.com/people/v2/people/" + person_id + "/emails")
+      email_url = URI(
+        "https://api.planningcenteronline.com/people/v2/people/" + person_id + "/emails"
+      )
       email_http = Net::HTTP.new(email_url.host, email_url.port)
       email_http.use_ssl = true
       email_request = Net::HTTP::Get.new(email_url)
-      email_request.basic_auth ENV['PLANNING_CENTER_APPLICATION_ID'], ENV['PLANNING_CENTER_SECRET']
+      email_request.basic_auth ENV['PLANNING_CENTER_APPLICATION_ID'],
+        ENV['PLANNING_CENTER_SECRET']
       email = JSON.parse(email_http.request(email_request).body)
       puts "email = "
 
@@ -42,13 +47,12 @@ class FetchUsers
       if email['meta']['total_count'] > 0
         people_hash[person_id.to_sym][:email] = email['data'][0]['attributes']['address']
       else
-        people_hash[person_id.to_sym][:email] = 'none'
+        people_hash[person_id.to_sym][:email] = nil
       end
 
     end
-
-
     people_hash
   end
+
 
 end

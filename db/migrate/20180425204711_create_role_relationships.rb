30fcda6dd7db09ef92ce65684cfac2639c7db75d
Jason Doggart <jasondoggart@gmail.com>
2018-04-27 14:41:10 -0400

    Update migrations for pg compatibility

--- db/migrate/20180425204711_create_role_relationships.rb
+++ db/migrate/20180425204711_create_role_relationships.rb
@@ -2,7 +2,7 @@ class CreateRoleRelationships < ActiveRecord::Migration[5.1]
   def change
     create_table :role_relationships do |t|
      t.references :role, foreign_key: true
-      t.references :member, foreign_key: true
+      t.references :team_member, foreign_key: true
 
       t.timestamps
     end

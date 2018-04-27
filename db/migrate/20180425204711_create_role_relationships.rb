a5a9542ddd3ffd8db0c28c7fd5432ef73f792239
Jason Doggart <jasondoggart@gmail.com>
2018-04-27 14:45:00 -0400

    Another change to migrations

--- db/migrate/20180425204711_create_role_relationships.rb
+++ db/migrate/20180425204711_create_role_relationships.rb
@@ -2,7 +2,7 @@ class CreateRoleRelationships < ActiveRecord::Migration[5.1]
   def change
     create_table :role_relationships do |t|
      t.references :role, foreign_key: true
-      t.references :team_member, foreign_key: true
+      t.integer :team_member_id
 
       t.timestamps
     end

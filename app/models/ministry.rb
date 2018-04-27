5ce3c02b61b7782f3055f582ec7d6b41f6391179
Jason Doggart <jasondoggart@gmail.com>
2018-04-27 16:15:14 -0400

    Add method to calculate percent apprenticing
    Add new method to Ministry that takes in a role type and calculates the
    % of that role type that has an apprentice.  Display this value on the
    ministry show page, in the statistics section.

--- app/models/ministry.rb
+++ app/models/ministry.rb
@@ -14,4 +14,16 @@ class Ministry < ApplicationRecord
   def number_of_members
     roles.where(role_type: 'Team Member').count
   end
+
+  def percent_having_apprentice(role_type)
+    total_roles = roles.where(role_type: role_type)
+    return 0 if total_roles.count == 0
+    i = 0
+    total_roles.each do |role|
+      if role.apprentices.count > 0
+        i += 1
+      end
+    end
+    (i.to_f/total_roles.count.to_f * 100).round(0)
+  end
 end

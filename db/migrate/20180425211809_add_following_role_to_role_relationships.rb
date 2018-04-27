8ab0aa2db1f96d99829c2e0d7b1cabd9e19f60e7
Jason Doggart <jasondoggart@gmail.com>
2018-04-27 14:47:57 -0400

    Updated migrations for pg

--- db/migrate/20180425211809_add_following_role_to_role_relationships.rb
+++ db/migrate/20180425211809_add_following_role_to_role_relationships.rb
@@ -1,5 +1,5 @@
 class AddFollowingRoleToRoleRelationships < ActiveRecord::Migration[5.1]
   def change
-    add_reference :role_relationships, :following_role, foreign_key: true
+    add_column :role_relationships, :following_role_id, :integer
   end
 end
